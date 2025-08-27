5efc1870a77a544ae0b8545b6ada95529f5b1fc2
sagites <chizzyikemba@gmail.com>
2025-08-27 07:25:31 +0100

    removed logging of the db url

--- apps/shop/src/config/db.js
+++ apps/shop/src/config/db.js
@@ -1,26 +1,9 @@
-// import mongoose from 'mongoose';
-
-// const DATABASE_URL = process.env.DATABASE_URL;
-
-// if (!DATABASE_URL) throw new Error("Please define DATABASE_URL");
-
-// let isConnected = false;
-
-// export default async function connectDB() {
-//   if (isConnected) return;
-//   const db = await mongoose.connect(MONGODB_URI);
-//   isConnected = db.connections[0].readyState;
-// }
-
-
-// lib/mongodb.js
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+// const MONGODB_URI = process.env.MONGODB_URI;
 
- console.log(MONGODB_URI);
 
-if (!MONGODB_URI) {
+if (!process.env.MONGODB_URI) {
   throw new Error("Please define the DATABASE_URL environment variable inside .env");
 }
 
